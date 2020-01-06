c8005028bf287acaee2cfbc4eaa10582e9480e6b
oluwadamilolaadebayo <goshensoftinc@gmail.com>
2020-01-06 12:55:29 +0100

    - added .env file

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,15 @@
 import {SET_PLATFORM} from "./actionTypes";
+import axios from 'axios'
+
+export const fetchPlatforms = () => {
+  return async (dispatch) => {
+    try {
+      const platforms = axios.get()
+    } catch (e) {
+    
+    }
+  }
+}
 
 export const setPlatforms = ({platforms}) => ({
   type: SET_PLATFORM,
