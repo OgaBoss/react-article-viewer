5bcd67861b516fc1d04a00ebe68fb11192ced1bd
oluwadamilolaadebayo <goshensoftinc@gmail.com>
2020-01-07 06:29:59 +0100

    fixed error with store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
-import store from './redux/store.js';
+import configureStore from './redux/configureStore.js';
 import './assets/css/tailwind.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const store = configureStore();
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
