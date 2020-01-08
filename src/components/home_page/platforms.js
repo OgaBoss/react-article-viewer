6c9da90b09a36fcf0734922c49fbd470690906e3
oluwadamilolaadebayo <goshensoftinc@gmail.com>
2020-01-08 13:57:15 +0100

    styling news playforms

--- src/components/home_page/platforms.js
+++ src/components/home_page/platforms.js
@@ -1,13 +1,19 @@
 import React, {useEffect} from 'react'
-
+import styles from './platform.module.css'
 const Platforms = ({platform_reducer: {platforms = [], loading, error}, fetchPlatforms}) => {
   useEffect(() => {
     fetchPlatforms()
   }, [fetchPlatforms])
   
   return (
-    <div className=''>
-    
+    <div className={`h-full ${styles.platforms}`}>
+      {platforms.length < 0 ? (
+        <div></div>
+      ) : (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   )
 };
